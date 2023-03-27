702d9bb63104c1c1e8a2276e86647f88e4b59463
Dmitriy Kotovskij <kotovskij.01@yandex.ru>
2023-03-27 15:17:37 +0300

    fix (BRI-31) | Добавил кнопку назад в основную вёрстку

--- packages/client/src/pages/error500/error500.tsx
+++ packages/client/src/pages/error500/error500.tsx
@@ -1,10 +1,15 @@
 import styles from './error500.module.scss'
-import { BtnBack } from '../../components/block/btn_back'
+
 export const Error500 = () => {
     return <div className={styles.page}>
                 <p className={styles.page__p}>Сервис временно недоступен</p>
                 <p className={styles.page__p}>Ведём работы</p>
-                <div className={styles.page__btn}><BtnBack/></div>
+                <div className={styles.page__btn}>
+                        <button className={styles.button}>
+                                <div className={styles.button__icon}></div> 
+                                Вернуться назад
+                        </button>
+                </div>
         </div>
 }
 
