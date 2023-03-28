89a1d4e52fa0c31ef57932a20beb9f1003946596
Dmitriy Kotovskij <kotovskij.01@yandex.ru>
2023-03-28 14:33:27 +0300

    feat (BRI-21) | Вёрстка страницы профиля

---
+++ packages/client/src/pages/profile/profile.tsx
@@ -0,0 +1,167 @@
+import styles from "./profile.module.scss"
+import iconEdit from "../../../public/images/icons/icon_edit_pencil.svg"
+import iconWin from "../../../public/images/icons/icon_verified_user.svg"
+import iconBtnBack from "../../../public/images/icons/Vector.svg"
+import { MouseEventHandler } from "react"
+import React from "react"
+type profile = {
+    avatar: string;
+    first_name: string;
+    second_name: string;
+    mail: string;
+    phone: string;
+    oldPassword:string;
+    newPassword:string;
+}
+
+export const Profile = (props: profile) => {
+
+    const [isTrottle, setTrottle] = React.useState("true")
+
+
+    const addEditElement: MouseEventHandler = () => {
+        const button_close = document.querySelector<HTMLButtonElement>(`.${styles.header__btnClose}`);
+        const userInfo__names = document.querySelectorAll<HTMLDivElement>(`.${styles.props}`);
+        const message = document.querySelector<HTMLDivElement>(`.${styles.message}`);
+        const userInfo__save = document.querySelector<HTMLButtonElement>(`.${styles.userInfo__save}`);
+        const body__nickname = document.querySelector<HTMLElement>(`.${styles.nickname}`);
+        const userInfo__editPass = document.querySelector<HTMLButtonElement>(`.${styles.userInfo__editPass}`);
+
+        if(userInfo__names !== null && body__nickname !== null) {
+            body__nickname.innerHTML = `<input  class=${styles.body__userInfo_input} />`
+            userInfo__names.forEach((e) => {
+                    e.innerHTML = `<input  class=${styles.body__userInfo_input} />`
+            })
+        }
+
+        if(button_close !== null 
+            && message !== null 
+            && userInfo__save !== null 
+            && userInfo__editPass !== null) 
+        {
+            button_close.style.display = "flex";
+            message.style.display = "block";
+            userInfo__save.disabled = false;
+            userInfo__editPass.disabled = true;
+        }
+      };
+
+
+    const deleteEditElement:MouseEventHandler = () => {
+        const button_close = document.querySelector<HTMLButtonElement>(`.${styles.header__btnClose}`);
+        const userInfo__names = document.querySelectorAll(`.${styles.props}`);
+        const message = document.querySelector<HTMLDivElement>(`.${styles.message}`);
+        const userInfo__save = document.querySelector<HTMLButtonElement>(`.${styles.userInfo__save}`);
+        const body__nickname = document.querySelector<HTMLElement>(`.${styles.nickname}`);
+        const userInfo__editPass = document.querySelector<HTMLButtonElement>(`.${styles.userInfo__editPass}`);
+
+        if(button_close !== null 
+            && message !== null 
+            && userInfo__save !== null 
+            && userInfo__editPass !== null) 
+        {
+            button_close.style.display = "none";
+            message.style.display = "none";
+            userInfo__save.disabled = true;
+            userInfo__editPass.disabled = false;
+        }
+
+        if(userInfo__names !== null && body__nickname !== null) {
+            body__nickname.innerHTML = ""
+            userInfo__names.forEach((e) => {
+                if(e instanceof HTMLDivElement) {
+                    e.innerHTML = ""
+                } 
+            });
+        }
+    }
+
+
+    const addModal:MouseEventHandler = () => {
+        const modal = document.querySelector<HTMLDivElement>(`.${styles.background}`);
+        if(isTrottle === "true") {
+            if(modal !== null) {
+                modal.style.display = "flex"
+                setTrottle("false");
+            }
+        }else {
+            if(modal !== null) {
+                modal.style.display = "none" 
+                setTrottle("true");
+            }
+        }
+    }
+
+    return <div className={styles.page}>
+                <div className={styles.body}>
+                    <div className={styles.body__header}>
+                        <p className={styles.header__p}>
+                            Профиль <button onClick={addEditElement} className={styles.header__btn} ><img src={iconEdit} className={styles.header__image}/></button>
+                        </p>
+                        <div className={styles.message}>Редактирование профиля!</div>
+                        <button className={styles.header__btnClose} onClick={deleteEditElement}>x</button>
+                    </div>
+                    <div className={styles.body__userInfo}>
+                        <form method="post" encType="multipart/form-data" className={styles.userInfo__avatar}>
+                            Аватар:
+                            <span className={styles.avatar}> 
+                            <label htmlFor="input__avatar" className={styles.form__label_avatar}>
+                                <input type="file" name="input__avatar" className={styles.form__input_avatar} accept="image/png, image/jpeg"/>
+                            </label>
+                            </span>
+                        </form>
+                        <form action="">
+                            <div className={styles.userInfo__names}>Имя: <div className={styles.props}>{props.first_name}</div></div>
+                            <div className={styles.userInfo__names}>Фамилия: <div className={styles.props}>{props.second_name}</div></div>
+                            <div className={styles.userInfo__names}>Почта: <div className={styles.props}>{props.mail}</div></div>
+                            <div className={styles.userInfo__names}>Номер телефона: <div className={styles.props}>{props.phone}</div></div>
+                            <input className={styles.userInfo__save} type="submit" disabled value="Сохранить"/>
+                        </form>
+                        <button className={styles.userInfo__editPass} onClick={addModal}>Изменить пароль</button>
+                    </div>
+                    <div className={styles.body__gameInfo}>
+                        <div className={styles.body__nickname}>Nickname:<div className={styles.nickname}></div></div>
+                        <div className={styles.gameInfo__achievements}>
+                            Achievements:
+                            <div className={styles.gameInfo__achievements_body}>
+                                <Achievements  imp_path={iconWin} achieve_text="Win x10"/>
+                                <Achievements  imp_path={iconWin} achieve_text="Win x10"/>
+                                <Achievements  imp_path={iconWin} achieve_text="Win x10"/>
+                                <Achievements  imp_path={iconWin} achieve_text="Win x10"/>
+                                <Achievements  imp_path={iconWin} achieve_text="Win x10"/>
+                                <Achievements  imp_path={iconWin} achieve_text="Win x10"/>
+                                <Achievements  imp_path={iconWin} achieve_text="Win x10"/>
+                                <Achievements  imp_path={iconWin} achieve_text="Win x10"/>
+                                <Achievements  imp_path={iconWin} achieve_text="Win x10"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    <div className={styles.softBar}>
+                            <button className={styles.softBar__btn}><img src={iconBtnBack} alt="" className={styles.softBar__icon} /></button> 
+                    </div>
+                    <div className={styles.background}>
+                        <div className={styles.modal}>
+                            <div className={styles.modal__header}>Изменение пароля!<button className={styles.modal__btn__close} onClick={addModal}>X</button></div> 
+                            <form action="" className={styles.modal__form}>
+                                <div className={styles.userInfo__password}>Старый пароль: <div className={styles.props__OldPassword}>{props.oldPassword} <input  className={styles.body__userInfo_input} /></div></div>
+                                <div className={styles.userInfo__password}>Новый пароль:  <div className={styles.props__NewPassword}>{props.newPassword} <input  className={styles.body__userInfo_input} /></div></div>
+                                <input className={styles.modal__save} type="submit" value="Сохранить" />
+                            </form>
+                        </div> 
+                    </div> 
+            </div>
+}
+
+
+type achieve = {
+    imp_path: string;
+    achieve_text:string;
+}
+
+const Achievements = (props:achieve) => {
+    return <div className={styles.achievements}>
+                <img src={props.imp_path} alt="" className={styles.achievements__icon} />
+                <p className={styles.achievements_p}>{props.achieve_text}</p>
+            </div>
+}
