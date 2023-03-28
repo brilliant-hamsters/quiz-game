f09785fb6075200bcf06bb43b96e56ce412c6368
Dmitriy Kotovskij <kotovskij.01@yandex.ru>
2023-03-28 16:46:24 +0300

    fix (BRI-30) | Изменение названия компонента

--- packages/client/src/pages/notFound/notFound.tsx
+++ packages/client/src/pages/notFound/notFound.tsx
@@ -1,6 +1,6 @@
-import styles from './error404.module.scss'
+import styles from './notFound.module.scss'
 
-export const Error404 = () => {
+export const NotFound = () => {
     return <div className={styles.page}>
                 <span className={styles.page__span}>
                     <div className={styles.span__icon}></div> 
