871018bc1ca7652a71a384d0ec7f7b15073fd9b0
Dmitriy Kotovskij <kotovskij.01@yandex.ru>
2023-03-28 12:00:57 +0300

    feat (BRI-20) | Создал ветку для Epic задачи

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './App.module.scss';
 
 export const App = () => {
-  return <div className={styles.app}>Вот тут будет жить ваше приложение :)</div>;
+  return <div className={styles.app}></div>;
 };
