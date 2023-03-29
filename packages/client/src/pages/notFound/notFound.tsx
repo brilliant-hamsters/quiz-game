c0a575f96d8d2514e1bb9f98aee3dce5c46652fb
Dmitriy Kotovskij <kotovskij.01@yandex.ru>
2023-03-29 15:15:27 +0300

    fix (BRI-30) | Мелкие правки по коду

--- packages/client/src/pages/notFound/notFound.tsx
+++ packages/client/src/pages/notFound/notFound.tsx
@@ -2,10 +2,10 @@ import styles from './notFound.module.scss'
 
 export const NotFound = () => {
     return <div className={styles.page}>
-                <span className={styles.page__span}>
+                <div className={styles.page__span}>
                     <div className={styles.span__icon}></div> 
                     <p>Поломалося!!((</p> 
-                </span>
+                </div>
                 <div className={styles.page__button}>
                     <button className={styles.button}>
                         <div className={styles.button__icon}></div> 
