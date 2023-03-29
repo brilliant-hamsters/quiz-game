a9ecbfbd7ec0bffe455aaa6f8c9e094af3fc731d
Efim <alf-2010-ges@yandex.ru>
2023-03-30 00:00:42 +0400

    feat (BRI-37) Инициализация Redux
    feat (BRI-27) Верстка старта игры

--- packages/client/src/routes/index.tsx
+++ packages/client/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { GamePage } from "../pages/GamePage";
-import { GameEnd } from '../pages/GameEnd'
+import { GameEnd } from '../pages/GameEnd';
+import { GameStart } from '../pages/GameStart';
 
 interface IRoute {
     /**
@@ -21,46 +22,46 @@ interface IRoute {
 }
 
 export const routes: IRoute[] = [
-  {
-    id: 'game',
-    path: '/',
-    component: GamePage,
-    isPrivate: true
-  },
-  {
-    id: 'start',
-    path: '/start',
-    component: () => <div>Start</div>,
-    isPrivate: true
-  },
-  {
-    id: 'end',
-    path: '/end',
-    component: GameEnd,
-    isPrivate: true
-  },
-  {
-    id: 'leaderBoard',
-    path: '/leader_board',
-    component: () => <div>Leader Board</div>,
-    isPrivate: true
-  },
-  {
-    id: 'forum',
-    path: '/forum',
-    component: () => <div>Forum</div>,
-    isPrivate: true
-  },
-  {
-    id: 'auth',
-    path: '/auth',
-    component: () => <div>Auth</div>,
-    isPrivate: false
-  },
-  {
-    id: 'signUp',
-    path: '/sign_up',
-    component: () => <div>Sign Up</div>,
-    isPrivate: false
-  }
+    {
+        id: 'game',
+        path: '/',
+        component: GamePage,
+        isPrivate: true
+    },
+    {
+        id: 'start',
+        path: '/start',
+        component: GameStart,
+        isPrivate: true
+    },
+    {
+        id: 'end',
+        path: '/end',
+        component: GameEnd,
+        isPrivate: true
+    },
+    {
+        id: 'leaderBoard',
+        path: '/leader_board',
+        component: () => <div>Leader Board</div>,
+        isPrivate: true
+    },
+    {
+        id: 'forum',
+        path: '/forum',
+        component: () => <div>Forum</div>,
+        isPrivate: true
+    },
+    {
+        id: 'auth',
+        path: '/auth',
+        component: () => <div>Auth</div>,
+        isPrivate: false
+    },
+    {
+        id: 'signUp',
+        path: '/sign_up',
+        component: () => <div>Sign Up</div>,
+        isPrivate: false
+    }
 ]
