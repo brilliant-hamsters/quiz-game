62443739fe0a132c7eea44b035b86612373dc98f
Dmitriy Kotovskij <kotovskij.01@yandex.ru>
2023-04-20 18:07:20 +0300

    test

--- packages/client/src/pages/Login/Login.test.tsx
+++ packages/client/src/pages/Login/Login.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen} from "@testing-library/react"
-import { Login } from './Login';
+import Login  from './Login';
 import { Provider } from 'react-redux';
 import store from '../../store';
 import '@testing-library/jest-dom'
