bde5cee74b80ee82ff4ac38bbd76091bf6ded259
Artemiy <artem2k17xia@yandex.ru>
2023-04-20 22:31:38 +0500

    fix (BRI-45) доработка воркера

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './index.scss'
+import { startServiceWorker } from './utils/serviceWorkers/initialServiceWorkers'
+
+startServiceWorker()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
